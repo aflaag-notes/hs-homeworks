ce0850abf3c914e18c76192b9d3f72a5bcededff
aflaag <alessio.bandiera02@gmail.com>
2024-03-13 19:03:54 +0100

    non ho finito

--- hw1/HW1-AlessioBandiera.hs
+++ hw1/HW1-AlessioBandiera.hs
@@ -16,12 +16,11 @@ myDropWhile p xs@(x:txs) = if p x then myDropWhile p txs else xs
 zapp (f:fs) (x:xs) = f x : zapp fs xs
 zapp _ _ = []
 
--- TODO: da controllare
-myZipWith1 f xs ys = zapp (repeat (\t -> f (fst t) (snd t))) (zip xs ys)
+myZipWith1 f xs ys = zapp (map f xs) ys
 
 
 -- ### Esercizio 2.2
--- TODO: da fare
+myZipWith2 f xs ys = map (\t -> f (fst t) (snd t)) (zip xs ys)
 
 
 -- ### Esercizio 2.3
@@ -35,11 +34,11 @@ myMap1 f = foldr ((:) . f) []
 
 
 -- ### Esercizio 3.1
--- O(n^2)
+-- O(n^3)
 prefissi1 [] = []
 prefissi1 xs = (prefissi1 $ reverse $ tail $ reverse xs) ++ [xs]
 
--- O(n)
+-- O(n^2)
 prefissi2Internals _ [] = []
 prefissi2Internals ss (x:xs) = suff : prefissi2Internals suff xs
     where suff = ss ++ [x]
@@ -48,7 +47,33 @@ prefissi2 xs = prefissi2Internals [] xs
 
 
 -- ### Esercizio 3.2
-segSommaS xs s = 
+suffissi xs@(_:txs) = xs : suffissi txs
+suffissi [] = []
+
+segSommaS [x] s = if x == s then [x] else []
+segSommaS xs@(_:txs) s = (filter (\l -> s == sum l) (prefissi2 xs)) : segSommaS txs s
+
+
+-- ### Esercizio 3.3
+
+
+
+-- ## Esercizio 
+-- part1Internals n x y M = if y < x then 
+--
+-- part1 n = part1Internals n 0 1 [[0 | _ <- take n [0..]] | _ <- take n [0..]]
+
+-- part1 0 0 = 1
+-- part1 0 _ = 0
+-- part1 j k = if j < k then prev else prev + part1 (j - 1) (k - j)
+--     where prev = part1 (j - 1) k
+
+-- part1
+
+-- ## Esercizio 4.2
+part2 n = if n <= 0 then 1 else sum (map (\x -> part2 (n - x)) [1..n])
 
 main :: IO ()
-main = do putStrLn $ show $ segSommaS [4, 2, 3, 4] 9
+-- main = do putStrLn $ show $ segSommaS [4, 2, 3, 4] 9
+-- main = do putStrLn $ show $ part2 4
+main = do putStrLn $ show $ myZipWith2 (+) [1, 2, 3] [4, 5, 6]
