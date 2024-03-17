4bb6474d3fb74875e312b440df1fca29265a47cd
aflaag <alessio.bandiera02@gmail.com>
2024-03-17 13:05:30 +0100

    confronted

--- hw1/HW1-AlessioBandiera.hs
+++ hw1/HW1-AlessioBandiera.hs
@@ -1,5 +1,3 @@
-import Data.List (sort)
-
 -- ### Esercizio 1.1
 myTakeWhile p [] = []
 myTakeWhile p (x:xs) = x : if p x then myTakeWhile p xs else []
@@ -9,12 +7,12 @@ myDropWhile p xs@(x:txs) = if p x then myDropWhile p txs else xs
 
 
 -- ### Esercizio 1.2
+-- O(n)
 myRemoveDupsOrd [x] = [x]
 myRemoveDupsOrd xs
     | zipped == [] = []
     | otherwise = fst zippedHead : snd zippedHead : map snd (tail zipped)
-    where sxs = sort xs
-          zipped = filter (uncurry $ \x y -> x /= y) (zip sxs (tail sxs))
+    where zipped = filter (uncurry $ \x y -> x /= y) (zip xs (tail xs))
           zippedHead = head zipped
 
 
@@ -30,7 +28,7 @@ myZipWith1 f xs ys = zapp (map f xs) ys
 
 
 -- ### Esercizio 2.2
-myZipWith2 f xs ys = map (uncurry $ f) (zip xs ys)
+myZipWith2 f xs ys = map (uncurry f) (zip xs ys)
 
 
 -- ### Esercizio 2.3
@@ -50,17 +48,13 @@ prefissi1 xs = (prefissi1 $ reverse $ tail $ reverse xs) ++ [xs]
 
 -- O(n^2)
 prefissi2Internals _ [] = []
-prefissi2Internals ss (x:xs) = suff : prefissi2Internals suff xs
-    where suff = ss ++ [x]
+prefissi2Internals ps (x:xs) = pref : prefissi2Internals pref xs
+    where pref = ps ++ [x]
 
 prefissi2 xs = prefissi2Internals [] xs
 
 
 -- ### Esercizio 3.2
--- suffissi xs@(_:txs) = xs : suffissi txs
--- suffissi [] = []
-
--- TODO: come dovrei usa suffissi?
 segSommaS [x] s = if x == s then [[x]] else []
 segSommaS xs@(_:txs) s = filter (\l -> s == sum l) (prefissi2 xs) ++ segSommaS txs s
 
@@ -70,17 +64,26 @@ segSommaS xs@(_:txs) s = filter (\l -> s == sum l) (prefissi2 xs) ++ segSommaS t
 
 
 -- ### Esercizio 4.1
--- TODO: da fare
+-- O(n^3)
+partInternals 0 _ _ = 1
+partInternals n j k
+    | j < k = 0
+    | j >= k = if j == k then 1 else 0 + sum [partInternals n (j - k) i | i <- [k..n]]
+
+part n = partInternals n (n + 1) 1
 
 
 -- ### Esercizio 4.2
-part2 n = if n <= 0 then 1 else sum (map (\x -> part2 (n - x)) [1..n])
+-- O(2^n)
+part2 n = if n <= 0 then 1 else sum [part2 (n - x) | x <- [1..n]]
 
 
 -- ### Esercizio 4.3
+-- O(2^n)
+-- TODO FILTRALE
 partsInternals l n = if n <= 0 then [l] else concat [partsInternals (l ++ [x]) (n - x) | x <- [1..n]]
 
-parts n = myRemoveDupsOrd $ map sort (partsInternals [] n)
+parts n = partsInternals [] n
 
 
 -- ### Esercizio 4.4
@@ -90,7 +93,7 @@ part3 n = length $ parts n
 
 main :: IO ()
 -- main = do putStrLn $ show $ segSommaS [4, 2, 3, 4] 9
-main = do putStrLn $ show $ part3 4
+-- main = do putStrLn $ show $ part2 3
 -- main = do putStrLn $ show $ myMap2 (+3) [1, 2, 3]
--- main = do putStrLn $ show $ myRemoveDupsOrd [5, 2, 1, 2, 5, 7, 2, 1, 2, 7]
--- main = do putStrLn $ show $ myRemoveDupsOrd [2, 1, 1, 1, 1]
+-- main = do putStrLn $ show $ myRemoveDupsOrd $ sort [5, 2, 1, 2, 5, 7, 2, 1, 2, 7]
+main = do putStrLn $ show $ parts 1
