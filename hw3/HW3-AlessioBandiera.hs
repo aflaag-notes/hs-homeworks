7c0bebccfd273bc1a93521a1ebb6483f5a18393c
aflaag <alessio.bandiera02@gmail.com>
2024-04-24 22:07:54 +0200

    this definition is circular

--- hw3/HW3-AlessioBandiera.hs
+++ hw3/HW3-AlessioBandiera.hs
@@ -14,23 +14,24 @@ tartaglia = [1] : map nextT tartaglia
 
 
 -- Esercizio 3
-removeNsAux _ _ [] = []
-removeNsAux n 1 (x:xs) = removeNsAux n n xs
-removeNsAux n m (x:xs) = x : removeNsAux n (m - 1) xs
-
 removeNs n = removeNsAux n n
-
-diag = diagAux 0
     where
-        diagAux n (xs:xss) = xs!!n : diagAux (n + 1) xss
+        removeNsAux _ _ [] = []
+        removeNsAux n 1 (x:xs) = removeNsAux n n xs
+        removeNsAux n m (x:xs) = x : removeNsAux n (m - 1) xs
 
-nextL n xs = removeNs (xs!!(n - 1)) xs
+luckyNumbers = luckyNumbersAux idxs [1..]
+    where
+        idxs = reverse (2 : tail luckyNumbers)
+        luckyNumbersAux [] ys = ys
+        luckyNumbersAux (x:xs) ys = removeNs x (luckyNumbersAux xs ys)
 
 main :: IO ()
 -- main = do putStrLn $ "Alessio Bandiera 1985878"
 -- main = do putStrLn $ show $ take 125 insonnia
 -- main = do putStrLn $ show $ take 5 tartaglia
--- main = do putStrLn $ show $ take 7 luckyNumbers
+main = do putStrLn $ show $ take 2 luckyNumbers
 -- main = do putStrLn $ show $ removeNs 2 (take 50 [1..])
 -- main = do putStrLn $ show $ take 20 $ nextL 3 $ nextL 2 (nextL 2 [1..])
-main = do putStrLn $ show $ take 20 $ removeNs 15 $ removeNs 13 $ removeNs 9 $ removeNs 7 $ removeNs 3 $ removeNs 2 [1..]
+-- main = do putStrLn $ show $ take 20 $ removeNs 9 $ removeNs 7 $ removeNs 3 $ removeNs 2 [1..]
+-- main = do putStrLn $ show $ take 20 $ nonLoSo [9, 7, 3, 2] [1..]
