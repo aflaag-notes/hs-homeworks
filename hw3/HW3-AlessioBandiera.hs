eb75dfa1cc2f98aa8a09600f80ddea6e7684c195
aflaag <alessio.bandiera02@gmail.com>
2024-04-25 09:33:01 +0200

    added types

--- hw3/HW3-AlessioBandiera.hs
+++ hw3/HW3-AlessioBandiera.hs
@@ -14,22 +14,27 @@ tartaglia = [1] : map nextT tartaglia
 
 
 -- ### Esercizio 3
-removeNs n = removeNsAux n n
-    where
-        removeNsAux _ _ [] = []
-        removeNsAux n 1 (x:xs) = removeNsAux n n xs
-        removeNsAux n m (x:xs) = x : removeNsAux n (m - 1) xs
-
-iterateEnum f x = iterateEnumAux f x 0
-    where
-        iterateEnumAux f x n = x : iterateEnumAux f (f x n) (n + 1)
-
+diag :: [[a]] -> [a]
 diag = diagAux 0
     where
         diagAux n (xs:xss) = xs!!n : diagAux (n + 1) xss
 
-nextL xs n = removeNs (xs!!(n + 1)) xs
+removeNths :: (Eq a, Num a) => a -> [b] -> [b]
+removeNths n = removeNthsAux n n
+    where
+        removeNthsAux _ _ [] = []
+        removeNthsAux n 1 (x:xs) = removeNthsAux n n xs
+        removeNthsAux n m (x:xs) = x : removeNthsAux n (m - 1) xs
 
+nextL :: (Eq a, Num a) => [a] -> Int -> [a]
+nextL xs n = removeNths (xs!!(n + 1)) xs
+
+iterateEnum :: Num b => (a -> b -> a) -> a -> [a]
+iterateEnum f x = iterateEnumAux f x 0
+    where
+        iterateEnumAux f x n = x : iterateEnumAux f (f x n) (n + 1)
+
+luckyNumbers :: [Int]
 luckyNumbers = diag (iterateEnum nextL [1,3..])
 
 main :: IO ()
@@ -37,7 +42,3 @@ main :: IO ()
 -- main = do putStrLn $ show $ take 125 insonnia
 -- main = do putStrLn $ show $ take 5 tartaglia
 main = do putStrLn $ show $ take 50 luckyNumbers
--- main = do putStrLn $ show $ removeNs 2 (take 50 [1..])
--- main = do putStrLn $ show $ take 20 $ nextL [1..] 0
--- main = do putStrLn $ show $ take 20 $ removeNs 9 $ removeNs 7 $ removeNs 3 $ removeNs 2 [1..]
--- main = do putStrLn $ show $ take 20 $ nonLoSo [9, 7, 3, 2] [1..]
