7905b2423d804d4cbab2d32387cd6248fa7b3f63
aflaag <alessio.bandiera02@gmail.com>
2024-05-15 21:22:48 +0200

    nothing

--- hw4/HW4-AlessioBandiera.hs
+++ hw4/HW4-AlessioBandiera.hs
@@ -1,18 +1,16 @@
-newtype ST s a = S (s -> (a, s))
+import Control.Monad.State
 
-app :: ST s a -> s -> (a, s)
-app (S f) = f
+-- ### Esercizio 2
+data BinTree a = Node a (BinTree a) (BinTree a) | Empty
+    deriving Show
 
-instance Functor (ST s) where
-    fmap f st = S (\s -> let (x, s') = app st s in (f x, s'))
+-- balancedNodes Empty = state (\n -> (([], 0), 0))
+balancedNodes Empty = state (\n -> (Empty, 0))
+balancedNodes (Node a sx dx) = do bsx <- balancedNodes sx
+                                  bdx <- balancedNodes dx
+                                  -- state (\n -> ((if n - totsx - totdx == totsx + totdx + a then a:bsx ++ bdx else bsx ++ bdx, totsx + totdx + a), n + a))
+                                  state (\n -> (Node n bsx bdx, n + a))
 
-instance Applicative (ST s) where
-    pure x = S (\s -> (x, s))
-    stf <*> stx = S (\s -> let (f, s') = app stf s in let (x, s'') = app stx s' in (f x, s''))
-
-instance Monad (ST s) where
-    stx >>= f = S (\s -> let (x, s') = app stx s in app (f x) s')
-    return = pure
 
 -- ### Esercizio 3
 data NatBin = Zero NatBin | One NatBin | End
@@ -45,17 +43,18 @@ safeDivNatBin u v = do a <- u
                        b <- v
                        if (fromNatBin b) /= 0 then pure (intoNatBin ((fromNatBin a) `div` (fromNatBin b))) else Nothing
 
-logNatBin :: Maybe NatBin -> NatBinExp -> ST (Maybe NatBinExpErr) (Maybe NatBin)
-logNatBin Nothing (Div x y) = S (\s -> (Nothing, Just ZeroDivision))
-logNatBin v _ = S (\s -> (v, s))
+logNatBin :: Maybe NatBin -> NatBinExp -> State (Maybe NatBinExpErr) (Maybe NatBin)
+logNatBin Nothing (Div x y) = state (\s -> (Nothing, Just ZeroDivision))
+logNatBin v _ = state (\s -> (v, s))
 
-eval :: NatBinExp -> ST (Maybe NatBinExpErr) (Maybe NatBin)
-eval (Const x) = S (\s -> (Just x, s))
+eval :: NatBinExp -> State (Maybe NatBinExpErr) (Maybe NatBin)
+eval (Const x) = state (\s -> (Just x, s))
 eval (Div x y) = do u <- eval x
                     v <- eval y
                     logNatBin (safeDivNatBin u v) (Div x y)
 
 main :: IO ()
 -- main = do putStrLn $ show $ "Alessio Bandiera"
-main = do putStrLn $ show $ let term = Div (Const (intoNatBin 6)) (Const (intoNatBin 3)) in app (eval term) Nothing
--- main = do putStrLn $ show $ let term = Div (Const (intoNatBin 6)) (Const (intoNatBin 0)) in app (eval term) Nothing
+main = do putStrLn $ show $ runState (balancedNodes (Node 1 (Node 7 (Node 5 (Node 3 Empty Empty) (Node 1 Empty (Node 1 Empty Empty))) Empty) (Node 3 (Node 2 (Node 1 Empty Empty) (Node 1 Empty Empty)) Empty))) 0
+-- main = do putStrLn $ show $ let term = Div (Const (intoNatBin 6)) (Const (intoNatBin 3)) in runState (eval term) Nothing
+-- main = do putStrLn $ show $ let term = Div (Const (intoNatBin 6)) (Const (intoNatBin 0)) in runState (eval term) Nothing
