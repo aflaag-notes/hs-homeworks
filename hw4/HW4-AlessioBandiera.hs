16cd221cbf10d776557d8bf2cb28f795a7dbf3f7
aflaag <alessio.bandiera02@gmail.com>
2024-05-17 19:11:19 +0200

    a lot of progress for 3 + adjusted 2

--- hw4/HW4-AlessioBandiera.hs
+++ hw4/HW4-AlessioBandiera.hs
@@ -1,4 +1,5 @@
 import Control.Monad.State
+import Data.Maybe (fromJust)
 
 -- ### Esercizio 2
 data BinTree a = Node a (BinTree a) (BinTree a) | Empty
@@ -30,62 +31,112 @@ f''' [] = pure []
 f''' (x:xs) = pure (:) <*> (pure (*) <*> get <*> pure x ) <*> (state (\s -> (id, s + 1)) <*> f''' xs)
 
 
-balancedNodes Empty = return []
-balancedNodes (Node a sx dx) = do (path, subtree) <- get
-                                  put (path + a, subtree)
-                                  bsx <- balancedNodes sx
-                                  (pathSx, subtreeSx) <- get
-                                  put (path + a, subtree)
-                                  bdx <- balancedNodes dx
-                                  (pathDx, subtreeDx) <- get
-                                  put (path + a, subtreeSx + subtreeDx + a)
-                                  return (if path == subtreeDx + subtreeSx + a then a:bsx ++ bdx else bsx ++ bdx)
+balancedNodes b = evalState (balancedNodesAux b) (0, 0)
+    where
+        balancedNodesAux Empty = return []
+        balancedNodesAux (Node a sx dx) = do (path, subtree) <- get
+                                             let totPath = path + a
+                                             put (totPath, subtree)
+                                             bsx <- balancedNodesAux sx
+                                             (pathSx, subtreeSx) <- get
+                                             put (totPath, subtree)
+                                             bdx <- balancedNodesAux dx
+                                             (pathDx, subtreeDx) <- get
+                                             let totSubtree = subtreeSx + subtreeDx + a
+                                             let bs = bsx ++ bdx
+                                             put (totPath, totSubtree)
+                                             return (if path == totSubtree then a:bs else bs)
 
 
 -- ### Esercizio 3
 data NatBin = Zero NatBin | One NatBin | End
     deriving (Eq, Ord, Show)
 
-data NatBinExp = Const NatBin | Div NatBinExp NatBinExp 
-    deriving Show
+lengthNatBin End = 0
+lengthNatBin (Zero x) = 1 + lengthNatBin x
+lengthNatBin (One x) = 1 + lengthNatBin x 
 
-data NatBinExpErr = ZeroDivision
-    deriving Show
+isByteNatBin n = lengthNatBin n <= 8
 
--- Add Term Term | Sub Term Term | Mul Term Term | Div Term Term | Mod Term Term
+isByte x = (x <= 255) && (x >= 0)
 
-fromNatBin :: NatBin -> Int
-fromNatBin x = fromNatBinAux x 0
+fromNatBin :: NatBin -> Maybe Int
+fromNatBin x = let n = fromNatBinAux x 0 in if isByte n then Just n else Nothing
     where
         fromNatBinAux (Zero x) i = fromNatBinAux x (i + 1)
         fromNatBinAux (One x) i = 1 * (2^i) + fromNatBinAux x (i + 1)
         fromNatBinAux End _ = 0
 
-intoNatBin :: Int -> NatBin
-intoNatBin n = intoNatBinAux (n `divMod` 2)
+intoNatBin :: Int -> Maybe NatBin
+intoNatBin n = if isByte n then Just (intoNatBinAux (n `divMod` 2)) else Nothing
     where
         intoNatBinAux (0, 0) = End
         intoNatBinAux (n, 0) = Zero (intoNatBinAux (n `divMod` 2))
         intoNatBinAux (n, 1) = One (intoNatBinAux (n `divMod` 2))
 
-safeDivNatBin :: Maybe NatBin -> Maybe NatBin -> Maybe NatBin
-safeDivNatBin u v = do a <- u
-                       b <- v
-                       if (fromNatBin b) /= 0 then pure (intoNatBin ((fromNatBin a) `div` (fromNatBin b))) else Nothing
+addNatBin m n = addNatBinAux m n 0
+    where
+        addNatBinAux End End 0 = End
+        addNatBinAux End End 1 = One End
+        addNatBinAux (Zero x) End 0 = Zero (addNatBinAux x End 0)
+        addNatBinAux (Zero x) End 1 = One (addNatBinAux x End 0)
+        addNatBinAux (One x) End 0 = One (addNatBinAux x End 0)
+        addNatBinAux (One x) End 1 = Zero (addNatBinAux x End 1)
+        addNatBinAux End (Zero x) 0 = Zero (addNatBinAux End x 0)
+        addNatBinAux End (Zero x) 1 = One (addNatBinAux End x 0)
+        addNatBinAux End (One x) 0 = One (addNatBinAux End x 0)
+        addNatBinAux End (One x) 1 = Zero (addNatBinAux End x 1)
+        addNatBinAux (Zero x) (Zero y) 0 = Zero (addNatBinAux x y 0)
+        addNatBinAux (Zero x) (Zero y) 1 = One (addNatBinAux x y 0)
+        addNatBinAux (Zero x) (One y) 0 = One (addNatBinAux x y 0)
+        addNatBinAux (Zero x) (One y) 1 = Zero (addNatBinAux x y 1)
+        addNatBinAux (One x) (Zero y) 0 = One (addNatBinAux x y 0)
+        addNatBinAux (One x) (Zero y) 1 = Zero (addNatBinAux x y 1)
+        addNatBinAux (One x) (One y) 0 = Zero (addNatBinAux x y 1)
+        addNatBinAux (One x) (One y) 1 = One (addNatBinAux x y 1)
 
-logNatBin :: Maybe NatBin -> NatBinExp -> State (Maybe NatBinExpErr) (Maybe NatBin)
-logNatBin Nothing (Div x y) = state (\s -> (Nothing, Just ZeroDivision))
-logNatBin v _ = state (\s -> (v, s))
+data Term = Const NatBin | Add Term Term -- | Sub Term Term | Mul Term Term | Div Term Term | Mod Term Term
+    deriving Show
 
-eval :: NatBinExp -> State (Maybe NatBinExpErr) (Maybe NatBin)
-eval (Const x) = state (\s -> (Just x, s))
-eval (Div x y) = do u <- eval x
-                    v <- eval y
-                    logNatBin (safeDivNatBin u v) (Div x y)
+data MaybeTerm a = JustTerm a | ZeroDivisionErr | NegativeNumberErr | OverflowErr
+    deriving Show
+
+instance Functor MaybeTerm where
+    fmap f ZeroDivisionErr = ZeroDivisionErr
+    fmap f NegativeNumberErr = NegativeNumberErr
+    fmap f OverflowErr = OverflowErr
+    fmap f (JustTerm x) = JustTerm (f x)
+
+instance Applicative MaybeTerm where
+    pure = JustTerm
+    ZeroDivisionErr <*> _ = ZeroDivisionErr
+    NegativeNumberErr <*> _ = NegativeNumberErr
+    OverflowErr <*> _ = OverflowErr
+    (JustTerm f) <*> mx = fmap f mx
+
+instance Monad MaybeTerm where
+    return = pure
+    ZeroDivisionErr >>= _ = ZeroDivisionErr
+    NegativeNumberErr >>= _ = NegativeNumberErr
+    OverflowErr >>= _ = OverflowErr
+    (JustTerm x) >>= f = f x
+
+fromJustTerm ZeroDivisionErr = error "MaybeTerm.fromJustTerm: ZeroDivisionErr"
+fromJustTerm NegativeNumberErr = error "MaybeTerm.fromJustTerm: NegativeNumberErr"
+fromJustTerm OverflowErr = error "MaybeTerm.fromJustTerm: OverflowErr"
+fromJustTerm (JustTerm x) = x
+
+evalTerm :: Term -> MaybeTerm NatBin
+evalTerm t = do res <- evalTermAux t
+                if isByteNatBin res then JustTerm res else OverflowErr
+    where
+        evalTermAux (Const x) = JustTerm x
+        evalTermAux (Add x y) = do m <- evalTermAux x
+                                   n <- evalTermAux y
+                                   if (isByteNatBin m && isByteNatBin n) then JustTerm (m `addNatBin` n) else OverflowErr
 
 main :: IO ()
 -- main = do putStrLn $ show $ "Alessio Bandiera"
--- main = do putStrLn $ show $ runState (balancedNodes (Node 1 (Node 7 (Node 5 (Node 1 Empty Empty) (Node 1 Empty (Node 1 Empty Empty))) Empty) (Node 3 (Node 2 (Node 1 Empty Empty) (Node 1 Empty Empty)) Empty))) (0, 0)
--- main = do putStrLn $ show $ let term = Div (Const (intoNatBin 6)) (Const (intoNatBin 3)) in runState (eval term) Nothing
--- main = do putStrLn $ show $ let term = Div (Const (intoNatBin 6)) (Const (intoNatBin 0)) in runState (eval term) Nothing
-main = do putStrLn $ show $ runState (f''' [1, 1, 1]) 1
+-- main = do putStrLn $ show $ [5, 2] == balancedNodes (Node 1 (Node 7 (Node 5 (Node 1 Empty Empty) (Node 1 Empty (Node 1 Empty Empty))) Empty) (Node 3 (Node 2 (Node 1 Empty Empty) (Node 1 Empty Empty)) Empty))
+main = do putStrLn $ show $ and [x + y == (fromJust $ fromNatBin $ fromJustTerm (evalTerm $ Add (Const $ fromJust $ intoNatBin x) (Const $ fromJust $ intoNatBin y))) | x <- [0..128], y <- [0..127]]
+-- main = do putStrLn $ show $ runState (f''' [1, 1, 1]) 1
